72f2086870f5f0eb31069002a3e58c45b7f08fcd
Ndima-karabo17 <mhangwanikarabo@gmail.com>
2025-07-22 08:18:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
-const xArray = ["IoT device connection status", "User Access Logs", "Startup repair", "Boot logs", "Recovering systems & disaster recovery"];
-const yArray = [55, 49, 44, 24, 15];
+window.addEventListener("DOMContentLoaded", () => {
 
-const layout = {title:"World Wide Wine Production"};
+  const xArray = [
+    "IoT device connection status",
+    "User Access Logs",
+    "Startup repair",
+    "Boot logs",
+    "Recovering systems & disaster recovery"
+  ];
+  const yArray = [55, 49, 44, 24, 15];
 
-const data = [{labels:xArray, values:yArray, type:"pie"}];
+  const pieData = [{
+    labels: xArray,
+    values: yArray,
+    type: "pie"
+  }];
 
-Plotly.newPlot("myPlot", data, layout);
+  const pieLayout = {
+    title: "Nexus Cloud Shield - System Components Distribution"
+  };
+
+  Plotly.newPlot("myPlot", pieData, pieLayout);
+
+
+  const xValues = [];
+  const yValues = [];
+
+  generateData("x * 2 + 7", 0, 10, 0.5);
+
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        fill: false,
+        pointRadius: 1,
+        borderColor: "rgba(255,0,0,0.5)",
+        data: yValues
+      }]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "y = x * 2 + 7",
+        fontSize: 16
+      }
+    }
+  });
+
+  function generateData(expr, start, end, step = 1) {
+    for (let x = start; x <= end; x += step) {
+      xValues.push(x);
+      yValues.push(eval(expr));
+    }
+  }
+});
